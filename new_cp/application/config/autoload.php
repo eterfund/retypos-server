e5c6b7a1178db55e4dc026888c221211bd75ca6d
Георгий Попов <ambulance@sandbox.eterhost.ru>
2017-03-10 17:49:26 +0300

    remove unnessesary models autoload

--- new_cp/application/config/autoload.php
+++ new_cp/application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('mdl_views', 'mdl_post', 'mdl_query', 'mdl_search', 'mdl_session', 'mdl_authorized', 'mdl_menu');
+$autoload['model'] = array();

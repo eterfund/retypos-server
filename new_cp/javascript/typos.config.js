081c97d9bc18f8f527b7b195bef63dda31da4587
George Popoff <ambulance@etersoft.ru>
2017-06-06 20:47:20 +0300

    change to real base path

--- new_cp/javascript/typos.config.js
+++ new_cp/javascript/typos.config.js
@@ -1,4 +1,3 @@
 var TYPOS = {
-    //base_url: 'http://eterfund.ru/api/typos/cp/',
-    base_url: 'http://ambulance.sandbox.eterhost.ru/typos.server/new_cp/',
+    base_url: 'http://eterfund.ru/api/typos/new_cp/'
 };

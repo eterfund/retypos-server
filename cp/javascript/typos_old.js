9d7758d2e2c486f0ead1380dde3e78695f13c911
George Popoff <popoff96@live.com>
2018-03-30 17:56:55 +0300

    Добавил старый js код

---
+++ cp/javascript/typos_old.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    Typos_old._init();
+});
+
+var Typos_old = {
+  _init: function() {
+      this.contextBlock = $("#context-block");
+      this.contextBlockText = this.contextBlock.children().first();
+  },
+  
+  /**
+   * Показывает окно, содержащее информацию о контексте.
+   * @param {type} link Элемент a.typos_context
+   * @returns {Boolean}
+   */
+  handleLink: function(link) {
+    var text = link.getAttribute("context");
+    var typo = link.getAttribute("typo");
+    var correct = link.getAttribute("correct");
+    var self = this;
+    
+    text = text.replace(typo, "<span class='typo'>" + typo + " -> " + correct + "</span>");
+    
+    self.contextBlockText.html(text);
+    self.contextBlock.dialog();
+    
+    return false;
+  } 
+};
+
+

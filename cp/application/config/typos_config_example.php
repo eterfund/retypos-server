3ab5ad9f85296c71ee80c31eda875ac245bbc442
Jorgen <jorgen@jorgen.Dlink>
2018-07-10 16:04:25 +0300

    Remove configuration. Add example typos config

--- cp/application/config/typos_config_example.php
+++ cp/application/config/typos_config_example.php
@@ -2,12 +2,13 @@
 
 /*TYPOS super admin*/
 $config['typos_admin_login'] = 'admin';
-$config['typos_admin_password'] = 'someCoolPassword';
-$config['typos_admin_email'] = 'info@etersoft.ru';
+$config['typos_admin_password'] = 'password';
+$config['typos_admin_email'] = 'email@admin.com';
 
-$config['pass']['key_1'] = "ghuHJGOuiар";
-$config['pass']['key_2'] = "vD%F&Bv8bcvjnb%65";
-$config['pass']['key_3'] = "&^^%$%$*Gposwорапо7д";
+// Admin passwords
+$config['pass']['key_1'] = "password1";
+$config['pass']['key_2'] = "password2";
+$config['pass']['key_3'] = "password3";
 
 $config['error_login_count'] = 3;
 $config['error_login_time'] = 600; //10 минут
@@ -15,4 +16,4 @@ $config['error_login_time'] = 600; //10 минут
 /* Typos credentials for external apis */
 $config['correction_path'] = "correctTypo";
 $config['typos_user'] = 'typos.etersoft';
-$config['typos_password'] = 'Ync~YC{jBamlfuw7N3kG?XkiMpn}l6YJ';
+$config['typos_password'] = '';

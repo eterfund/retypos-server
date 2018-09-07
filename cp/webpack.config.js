ec0f6ac41d6673ba903d919edb735f5220925dd3
George Popoff <ambulance@etersoft.ru>
2018-09-07 19:40:50 +0300

    Заменил пресет es-2015 на env и добавил плагины для обработки свойств класса

--- cp/webpack.config.js
+++ cp/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 module.exports = {
     entry: [
         // Application code
-        "./javascript/src/typos.jsx",
+        "./javascript/src/typos.jsx"
     ],
 
     output: {
